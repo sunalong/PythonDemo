0bb34e2ad585c22078beff84351b0d6cc43bcb89
sunalong <sunalong@ztgame.com>
2018-09-03 12:09:42 +0800

    cgi Todo

---
+++ com/itcode/prammer/cgiTest.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+#coding:utf-8
+# CGI由NCSA维护，NCSA定义CGI如下：
+# CGI(Common Gateway Interface),通用网关接口，这是一段程序，运行在服务器上如：HTTP服务器，提供同客户端HTML页面的接口
+# CGI程序可以是Python脚本，PERL脚本，SHELL脚本，C或C++程序等
+# Web服务器支持及配置
