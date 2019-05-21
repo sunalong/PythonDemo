ef43b98126812e49673e9486688754e07806a19a
sunalong <466210864@qq.com>
2019-05-21 14:23:43 +0800

    语法修改

--- com/itcode/grammer/socketClientTest.py
+++ com/itcode/grammer/socketClientTest.py
@@ -6,5 +6,5 @@ s = socket.socket()
 host = socket.gethostname()
 port = 12345
 s.connect((host,port))
-print "客户端接收到的数据：",s.recv(1024)
+print("客户端接收到的数据：",s.recv(1024))
 s.close()
--- com/itcode/prammer/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-#coding:UTF-8
-if __name__ == '__main__':
-    print "作为主程序运行"
-else:
-    print "package_prammer 初始化"
