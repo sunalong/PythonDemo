6228c6a3614acd57d899bddd47c63215046c9a05
sunalong <466210864@qq.com>
2019-06-14 21:42:16 +0800

    mport导入模块同时指定别名

---
+++ com/itcode/grammer/6_模块/2_import导入模块同时指定别名.py
@@ -0,0 +1,12 @@
+import module_1_测试模块 as DogModule
+import module_2_测试模块 as CatModule
+
+DogModule.say_hello()
+CatModule.say_hello()
+
+dog = DogModule.Dog()
+print(dog)
+
+
+cat = CatModule.Cat()
+print(cat)
