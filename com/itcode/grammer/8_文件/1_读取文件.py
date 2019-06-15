f827b574c15585879e3e0299e944cbabec1c99df
sunalong <466210864@qq.com>
2019-06-15 19:07:52 +0800

    读取文件

--- com/itcode/grammer/8_文件/1_读取文件.py
+++ com/itcode/grammer/8_文件/1_读取文件.py
@@ -4,6 +4,12 @@ file = open("README")
 # 2.读取文件内容
 text = file.read()
 print(text)
-
+print(len(text))
+print("读取文件后 文件指针会改变")
+# 读取文件后 文件指针会改变
+text = file.read()
+print(text)
+print(len(text))
 # 3.关闭文件
 file.close()
+
